7954df49bb2e38d4322b849115988971a422d300
Mark Leo Jalandoni <95608455+markyleo@users.noreply.github.com>
2023-06-20 21:18:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello_scrape():
 
 @app.route('/keywords/', methods=['GET'])
 def twitter_keyword():
-    keyword_qry = str(request.args.get('keywords'))
+    keyword_qry = str(request.args.get('query'))
     threshold = 1
     if int(request.args.get('threshold')) > 1 :
         threshold = int(request.args.get('threshold'))
@@ -22,14 +22,12 @@ def twitter_keyword():
     tweets = []
 
     for i, tweet in enumerate(scraper.get_items()):
-        data_set = {'id': tweet.id, 'user' : tweet.user.displayname, 'date' : str(tweet.date) ,'content' : str(tweet.rawContent),'username' : tweet.user.username, 'like_count' : tweet.likeCount, 'retweet_count' : tweet.retweetCount}
+        data_set = {'id': tweet.id, 'user' : tweet.user.displayname, 'date' : str(tweet.date) ,'content' : str(tweet.rawContent),'media' : str(tweet.media,'username') : tweet.user.username, 'like_count' : tweet.likeCount, 'retweet_count' : tweet.retweetCount}
         if int(tweet.retweetCount) > int(threshold) :
             tweets.append(data_set)
-        if i > 300 :
+        if i > 100 :
             break
-
-    #tweet_dfs = pd.DataFrame(tweets, columns=["twitter_id","display_name","date","content","username","like_count","retweet_count"])
-    #tweet_df = json.dumps(tweets)
+            
     print(str(json.dumps(tweets)))
     return str(json.dumps(tweets))
 
