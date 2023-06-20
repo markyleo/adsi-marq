4236f6cba22fb8394590432e6a9ce94ba45b6f10
Mark Leo Jalandoni <95608455+markyleo@users.noreply.github.com>
2023-06-21 01:56:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,25 +12,25 @@ def hello_scrape():
 @app.route('/keywords/', methods=['GET'])
 def twitter_keyword():
     keyword_qry = str(request.args.get('query'))
-    threshold = 0
-    if int(request.args.get('threshold')) > 0 :
+    threshold = 1
+    if int(request.args.get('threshold')) >= threshold :
         threshold = int(request.args.get('threshold'))
 
-    print(threshold) 
+    print(threshold)
 
     scraper = sntwitter.TwitterSearchScraper("'"+keyword_qry+"'")
     tweets = []
 
     for i, tweet in enumerate(scraper.get_items()):
-        data_set = {'id': tweet.id, 'user' : tweet.user.displayname, 'date' : str(tweet.date) ,'content' : str(tweet.rawContent),'media' : str(tweet.media),'username' : tweet.user.username, 'like_count' : tweet.likeCount, 'retweet_count' : tweet.retweetCount}
+        data_set = {'id': tweet.id, 'user' : tweet.user.displayname, 'date' : tweet.date ,'content' : tweet.rawContent,'url' : tweet.url,'media' : tweet.media,'username' : tweet.user.username, 'like_count' : tweet.likeCount, 'retweet_count' : tweet.retweetCount}
         if int(tweet.retweetCount) >= int(threshold) :
             tweets.append(data_set)
         if i > 200 :
             break
-            
+
     print(jsonify(tweets))
     return jsonify(tweets)
 
 
-if __name__ == '__main__' : 
+if __name__ == '__main__' :
     app.run()
