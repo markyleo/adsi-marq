731838d2beac757be1c98ed18d8c8107b4c53de3
Mark Leo Jalandoni <95608455+markyleo@users.noreply.github.com>
2023-07-11 17:22:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import *
 #import pandas as pd
 #import json
+from datetime import date
 import snscrape.modules.twitter as sntwitter
 
 app = Flask(__name__)
@@ -9,16 +10,17 @@ app = Flask(__name__)
 def hello_scrape():
     return 'SNS-Twitter-Scrape'
 
-@app.route('/keywords/', methods=['GET'])
+@app.route('/keywords/', methods=['GET'], strict_slashes=False)
 def twitter_keyword():
     keyword_qry = str(request.args.get('query'))
     threshold = 1
+    today = str(date.today())
     if int(request.args.get('threshold')) >= threshold :
         threshold = int(request.args.get('threshold'))
 
-    print(threshold)
+    print(threshold) 
 
-    scraper = sntwitter.TwitterSearchScraper("'"+keyword_qry+"'")
+    scraper = sntwitter.TwitterSearchScraper("'"+keyword_qry+" since:"+today+"'")
     tweets = []
 
     for i, tweet in enumerate(scraper.get_items()):
@@ -27,10 +29,10 @@ def twitter_keyword():
             tweets.append(data_set)
         if i > 200 :
             break
-
+            
     print(jsonify(tweets))
     return jsonify(tweets)
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__' : 
     app.run()
