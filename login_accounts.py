a38ec8ddd0977ebb73ae53ff7105c6089f3f6783
fborlas-adish <francis.borlas@adish.com.ph>
2024-09-25 18:32:36 +0800

    integrate aws ses

--- login_accounts.py
+++ login_accounts.py
@@ -5,6 +5,11 @@ from twscrape.logger import set_log_level
 import time
 import requests
 import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from email.mime.multipart import MIMEMultipart
 
 os.environ["TWS_PROXY"] = "http://IbDxpBQwzg6vkEvu:8fsjGZeNV2YoDtY4@geo.iproyal.com:12321" # randomize per request
 # os.environ["TWS_PROXY"] = "http://IbDxpBQwzg6vkEvu:8fsjGZeNV2YoDtY4_session-eGfaqqcM_lifetime-30s@geo.iproyal.com:12321" # sticky IP
@@ -55,21 +60,29 @@ async def main():
     counter = 0
     while counter < 10:
         print(f"========================= Login all accounts retries: {counter + 1} =========================")
-        # await api.pool.login_all()
+        await api.pool.login_all()
         stats = await api.pool.stats()
         inactive = stats["inactive"]
         if inactive < 1:
             counter = 11
         else:
-            time.sleep(30)
+            time.sleep(15)
             counter += 1
     
     print("========================= Retries executed =========================")
     print(stats)
-    send_email_smtp("francis.borlas@adish.com.ph", "francis.borlas.9398@gmail.com", "Twitter Scraper Account Login Logs", "The bodyyy")
+
+    send_email_smtp(
+        "francis.borlas@adish.com.ph", 
+        "francis.borlas.9398@gmail.com", 
+        "X Scraper Account Login Logs", 
+        "Hi, attached here is the cron job logs for X scraper's account login.",
+        file_path="/home/ubuntu/app/adsi-marq/test_cron_log.txt"
+    )
+    
     del os.environ["TWS_PROXY"]
 
-def send_email_smtp(sender_email, receiver_email, subject, body):
+def send_email_smtp(sender_email, receiver_email, subject, body, file_path=None):
     # Create a multipart message and set headers
     message = MIMEMultipart()
     message['From'] = sender_email
@@ -78,6 +91,28 @@ def send_email_smtp(sender_email, receiver_email, subject, body):
 
     # Add body to email
     message.attach(MIMEText(body, 'plain'))
+
+    # Attach the file, if a file path is provided
+    if file_path:
+        try:
+            with open(file_path, 'rb') as attachment:
+                # Create a MIMEBase instance and set the payload as the attachment
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload(attachment.read())
+            
+            # Encode the payload using base64
+            encoders.encode_base64(part)
+
+            # Add header to the attachment
+            part.add_header(
+                'Content-Disposition',
+                f'attachment; filename={os.path.basename(file_path)}',
+            )
+
+            # Attach the file to the message
+            message.attach(part)
+        except Exception as e:
+            print(f"Failed to attach the file. Error: {str(e)}")
 
     # Create SMTP session for sending the mail
     try:
