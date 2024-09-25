d1df3a902a4377b4656d5d7c9690558215955d94
fborlas-adish <francis.borlas@adish.com.ph>
2024-09-25 18:15:57 +0800

    aws ses integration

--- login_accounts.py
+++ login_accounts.py
@@ -8,7 +8,11 @@ import os
 
 os.environ["TWS_PROXY"] = "http://IbDxpBQwzg6vkEvu:8fsjGZeNV2YoDtY4@geo.iproyal.com:12321" # randomize per request
 # os.environ["TWS_PROXY"] = "http://IbDxpBQwzg6vkEvu:8fsjGZeNV2YoDtY4_session-eGfaqqcM_lifetime-30s@geo.iproyal.com:12321" # sticky IP
- 
+
+os.environ["SMTP_USERNAME"] = "AKIASNNFRYSNJDMCIF5W"
+os.environ["SMTP_PASSWORD"] = "BGwhDgbH8Xx11HpqfxDwitmge10cVutt0Ply0BWvUlwG"
+os.environ["SMTP_ENDPOINT"] = "email-smtp.ap-northeast-1.amazonaws.com"
+
 def read_json_file(file_path):
     try:
         with open(file_path, 'r') as file:
@@ -51,7 +55,7 @@ async def main():
     counter = 0
     while counter < 10:
         print(f"========================= Login all accounts retries: {counter + 1} =========================")
-        await api.pool.login_all()
+        # await api.pool.login_all()
         stats = await api.pool.stats()
         inactive = stats["inactive"]
         if inactive < 1:
@@ -62,7 +66,35 @@ async def main():
     
     print("========================= Retries executed =========================")
     print(stats)
+    send_email_smtp("francis.borlas@adish.com.ph", "francis.borlas.9398@gmail.com", "Twitter Scraper Account Login Logs", "The bodyyy")
     del os.environ["TWS_PROXY"]
+
+def send_email_smtp(sender_email, receiver_email, subject, body):
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = subject
+
+    # Add body to email
+    message.attach(MIMEText(body, 'plain'))
+
+    # Create SMTP session for sending the mail
+    try:
+        username = os.environ["SMTP_USERNAME"]
+        password = os.environ["SMTP_PASSWORD"]
+        endpoint = os.environ["SMTP_ENDPOINT"]
+
+        server = smtplib.SMTP(endpoint, 587)
+        server.starttls()
+        server.login(username, password)
+        text = message.as_string()
+        server.sendmail(sender_email, receiver_email, text)
+        print('Email sent successfully!')
+    except Exception as e:
+        print(f'Failed to send email. Error: {str(e)}')
+    finally:
+        server.quit()  # Quit the SMTP session
 
 if __name__ == "__main__":
     asyncio.run(main())
