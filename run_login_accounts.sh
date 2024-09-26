b604772069a9a5b3c4586412037c1545d0689a64
fborlas-adish <francis.borlas@adish.com.ph>
2024-09-27 00:23:01 +0800

    update script

--- run_login_accounts.sh
+++ run_login_accounts.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# Delete the file first
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+echo "Running run_login_accounts.sh on $(date)"
+
+# Check if the file exists before attempting to delete
 if [ -f "/home/ubuntu/app/adsi-marq/accounts.db" ]; then
-    echo "Deleting /home/ubuntu/app/adsi-marq/accounts.db..."
+    echo "File /home/ubuntu/app/adsi-marq/accounts.db exists. Deleting..."
     rm /home/ubuntu/app/adsi-marq/accounts.db
+    echo "File deleted."
+else
+    echo "File /home/ubuntu/app/adsi-marq/accounts.db does not exist."
 fi
 
 # Run the Python script
