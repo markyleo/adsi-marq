d452442a9bcd0f51c36e38e0f1ce192e6180b3a1
fborlas-adish <francis.borlas@adish.com.ph>
2024-09-27 01:05:07 +0800

    update script

--- run_login_accounts.sh
+++ run_login_accounts.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-echo "Running run_login_accounts.sh on $(date)"
+echo "Running run_login_accounts.sh on $(date)" >> /home/ubuntu/app/adsi-marq/test_cron_log.txt
 
 # Check if the file exists before attempting to delete
 if [ -f "/home/ubuntu/app/adsi-marq/accounts.db" ]; then
-    echo "File /home/ubuntu/app/adsi-marq/accounts.db exists. Deleting..."
+    echo "File /home/ubuntu/app/adsi-marq/accounts.db exists. Deleting..." >> /home/ubuntu/app/adsi-marq/test_cron_log.txt
     rm /home/ubuntu/app/adsi-marq/accounts.db
-    echo "File deleted."
+    echo "File deleted." >> /home/ubuntu/app/adsi-marq/test_cron_log.txt
 else
-    echo "File /home/ubuntu/app/adsi-marq/accounts.db does not exist."
+    echo "File /home/ubuntu/app/adsi-marq/accounts.db does not exist." >> /home/ubuntu/app/adsi-marq/test_cron_log.txt
 fi
 
 # Run the Python script
-/home/ubuntu/app/adsi-marq/venv/bin/python3 /home/ubuntu/app/adsi-marq/login_accounts.py
+/home/ubuntu/app/adsi-marq/venv/bin/python3 /home/ubuntu/app/adsi-marq/login_accounts.py > /home/ubuntu/app/adsi-marq/test_cron_log.txt 2>&1
